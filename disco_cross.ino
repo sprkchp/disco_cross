42c08fae5fd4bfe7b85237a485a6da3bfdb92bed
sprkchp <176930083+sprkchp@users.noreply.github.com>
2025-08-23 13:13:38 +0200

    Switch to LED ring
    Working with the LED strip is a little but more complicated than I expected. Because I want to figure out the mechanics first, I'm gonna do it with the LED ring for now.

--- disco_cross.ino
+++ disco_cross.ino
@@ -10,12 +10,22 @@ void setup() {
   strip.Show();
   
   // Turn on all LEDs to bright red
-  for(int i = 0; i < NUM_LEDS; i++) {
-    strip.SetPixelColor(i, RgbColor(255, 0, 0));
-  }
-  strip.Show();
+  // for(int i = 0; i < NUM_LEDS; i++) {
+  //   strip.SetPixelColor(i, RgbColor(255, 0, 0));
+  // }
+  // strip.Show();
 }
 
 void loop() {
-  // Nothing needed here
+  // Random color animation - adapted from your Adafruit code
+  int randomLED = random(NUM_LEDS);
+  int randomR = random(255);
+  int randomG = random(255);
+  int randomB = random(255);
+  
+  strip.SetPixelColor(randomLED, RgbColor(randomR, randomG, randomB));
+  strip.Show();
+  
+  // Use a shorter delay to avoid timing issues
+  delay(100);
 }
